ad865d372c10feead73ec9069297e642ba4e9bd0
thebigsmileXD <thebigsmilexd@gmail.com>
2017-11-09 16:44:31 +0100

    Fix Brushes & Add experimental sphere brush

--- src/xenialdan/MagicWE2/shape/Square.php
+++ src/xenialdan/MagicWE2/shape/Square.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
 namespace xenialdan\MagicWE2\shape;
 
 
@@ -39,13 +20,18 @@ class Square extends Shape{
 		parent::__construct($level, $options);
 	}
 
-	public function getBlocksXYZ(Block ...$filterblocks){
+	/**
+	 * @param int $flags
+	 * @param Block[] ...$filterblocks
+	 * @return array
+	 */
+	public function getBlocks(int $flags, Block ...$filterblocks){
 		$blocks = [];
 		for ($x = $this->getMinVec3()->getX(); $x < $this->getMaxVec3()->getX(); $x++){
 			for ($z = $this->getMinVec3()->getZ(); $z < $this->getMaxVec3()->getZ(); $z++){
 				for ($y = $this->getMinVec3()->getY(); $y < $this->getMaxVec3()->getY(); $y++){
 					if (API::hasFlag($this->flags, API::FLAG_HOLLOW) && ($x > $this->getMinVec3()->getX() && $x < $this->getMaxVec3()->getX() - 1) && ($y > $this->getMinVec3()->getY() && $y < $this->getMaxVec3()->getY() - 1) && ($z > $this->getMinVec3()->getZ() && $z < $this->getMaxVec3()->getZ() - 1)) continue;
-					$blocks[(int)floor($x)][(int)floor($y)][(int)floor($z)] = new Vector3((int)floor($x), (int)floor($y), (int)floor($z));
+					$blocks[] = new Vector3((int)floor($x), (int)floor($y), (int)floor($z));
 				}
 			}
 		}
