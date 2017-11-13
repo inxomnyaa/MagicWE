e5a8a61dc4af96ae7350720ac177d8195ceaa68e
thebigsmileXD <thebigsmilexd@gmail.com>
2017-11-13 13:19:16 +0100

    Add undo/redo commands

---
+++ src/xenialdan/MagicWE2/commands/UndoCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace xenialdan\MagicWE2\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use xenialdan\MagicWE2\API;
+use xenialdan\MagicWE2\Loader;
+use xenialdan\MagicWE2\Selection;
+use xenialdan\MagicWE2\Session;
+use xenialdan\MagicWE2\WEException;
+
+class UndoCommand extends WECommand{
+	public function __construct(Plugin $plugin){
+		parent::__construct("/undo", $plugin);
+		$this->setPermission("we.command.undo");
+		$this->setDescription("Undos the last action");
+		$this->setUsage("//undo");
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+		/** @var Player $sender */
+		$return = $sender->hasPermission($this->getPermission());
+		if (!$return){
+			$sender->sendMessage(new TranslationContainer(TextFormat::RED . "%commands.generic.permission"));
+			return true;
+		}
+		$lang = Loader::getInstance()->getLanguage();
+		try{
+			/** @var Session $session */
+			$session = API::getSession($sender);
+			API::undo($session);
+		} catch (WEException $error){
+			$sender->sendMessage(Loader::$prefix . TextFormat::RED . "Looks like you are missing an argument or used the command wrong!");
+			$sender->sendMessage(Loader::$prefix . TextFormat::RED . $error->getMessage());
+			$return = false;
+		} catch (\Error $error){
+			$this->getPlugin()->getLogger()->error($error->getMessage());
+			$return = false;
+		} finally{
+			return $return;
+		}
+	}
+}
