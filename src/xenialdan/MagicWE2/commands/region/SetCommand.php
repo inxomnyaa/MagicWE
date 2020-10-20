bddaab31a4678ae9b97abfe438651a8412c89b1e
Restyled.io <commits@restyled.io>
2020-10-20 14:15:28 +0000

    Restyled by php-cs-fixer

--- src/xenialdan/MagicWE2/commands/region/SetCommand.php
+++ src/xenialdan/MagicWE2/commands/region/SetCommand.php
@@ -23,21 +23,21 @@ use xenialdan\MagicWE2\Loader;
 class SetCommand extends BaseCommand
 {
 
-	/**
-	 * This is where all the arguments, permissions, sub-commands, etc would be registered
-	 * @throws ArgumentOrderException
-	 * @throws InvalidArgumentException
-	 */
-	protected function prepare(): void
-	{
-		$this->registerArgument(0, new RawStringArgument("blocks", true));
-		$this->registerArgument(1, new TextArgument("flags", false));
-		$this->setPermission("we.command.region.set");
-	}
+    /**
+     * This is where all the arguments, permissions, sub-commands, etc would be registered
+     * @throws ArgumentOrderException
+     * @throws InvalidArgumentException
+     */
+    protected function prepare(): void
+    {
+        $this->registerArgument(0, new RawStringArgument("blocks", true));
+        $this->registerArgument(1, new TextArgument("flags", false));
+        $this->setPermission("we.command.region.set");
+    }
 
-	/**
-	 * @param CommandSender $sender
-	 * @param string $aliasUsed
+    /**
+     * @param CommandSender $sender
+     * @param string $aliasUsed
      * @param BaseArgument[] $args
      */
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
@@ -57,34 +57,34 @@ class SetCommand extends BaseCommand
         try {
             $messages = [];
             $error = false;
-			$replaceBlocks = API::blockParser((string)$args["blocks"], $messages, $error);
+            $replaceBlocks = API::blockParser((string)$args["blocks"], $messages, $error);
             foreach ($messages as $message) {
                 $sender->sendMessage($message);
             }
             if (!$error) {
                 $session = SessionHelper::getUserSession($sender);
                 if (is_null($session)) {
-					throw new SessionException($lang->translateString('error.nosession', [Loader::getInstance()->getName()]));
+                    throw new SessionException($lang->translateString('error.nosession', [Loader::getInstance()->getName()]));
                 }
-				$selection = $session->getLatestSelection();
-				if (is_null($selection)) {
-					throw new SelectionException($lang->translateString('error.noselection'));
-				}
-				if (!$selection->isValid()) {
-					throw new SelectionException($lang->translateString('error.selectioninvalid'));
-				}
-				if ($selection->getWorld() !== $sender->getWorld()) {
-					$sender->sendMessage(Loader::PREFIX . TF::GOLD . $lang->translateString('warning.differentworld'));
-				}
-				$hasFlags = isset($args["flags"]);
-				API::fillAsync($selection, $session, $replaceBlocks, $hasFlags ? API::flagParser(explode(" ", (string)$args["flags"])) : API::FLAG_BASE);
-			} else {
-				throw new InvalidArgumentException("Could not fill with the selected blocks");
-			}
-		} catch (Exception $error) {
-			$sender->sendMessage(Loader::PREFIX . TF::RED . $lang->translateString('error.command-error'));
-			$sender->sendMessage(Loader::PREFIX . TF::RED . $error->getMessage());
-			$sender->sendMessage($this->getUsage());
-		}
-	}
+                $selection = $session->getLatestSelection();
+                if (is_null($selection)) {
+                    throw new SelectionException($lang->translateString('error.noselection'));
+                }
+                if (!$selection->isValid()) {
+                    throw new SelectionException($lang->translateString('error.selectioninvalid'));
+                }
+                if ($selection->getWorld() !== $sender->getWorld()) {
+                    $sender->sendMessage(Loader::PREFIX . TF::GOLD . $lang->translateString('warning.differentworld'));
+                }
+                $hasFlags = isset($args["flags"]);
+                API::fillAsync($selection, $session, $replaceBlocks, $hasFlags ? API::flagParser(explode(" ", (string)$args["flags"])) : API::FLAG_BASE);
+            } else {
+                throw new InvalidArgumentException("Could not fill with the selected blocks");
+            }
+        } catch (Exception $error) {
+            $sender->sendMessage(Loader::PREFIX . TF::RED . $lang->translateString('error.command-error'));
+            $sender->sendMessage(Loader::PREFIX . TF::RED . $error->getMessage());
+            $sender->sendMessage($this->getUsage());
+        }
+    }
 }
