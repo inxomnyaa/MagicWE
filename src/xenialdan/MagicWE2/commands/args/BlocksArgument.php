0edf57388e7b3203a8eddcfffdd7c96dfb410306
XenialDan <thebigsmilexd@gmail.com>
2022-07-07 15:54:49 +0200

    Fixed <blocks> argument in commands - The blocks argument will show up and parse correctly again

--- src/xenialdan/MagicWE2/commands/args/BlocksArgument.php
+++ src/xenialdan/MagicWE2/commands/args/BlocksArgument.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace xenialdan\MagicWE2\commands\args;
 
-use CortexPE\Commando\args\RawStringArgument;
+use CortexPE\Commando\args\BaseArgument;
 use InvalidArgumentException as InvalidArgumentExceptionAlias;
 use pocketmine\command\CommandSender;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
 use xenialdan\MagicWE2\exception\BlockQueryAlreadyParsedException;
@@ -15,11 +16,16 @@ use xenialdan\MagicWE2\helper\BlockPalette;
 use xenialdan\MagicWE2\helper\SessionHelper;
 use xenialdan\MagicWE2\Loader;
 
-class BlocksArgument extends RawStringArgument
-{
+class BlocksArgument extends BaseArgument{
+	public function getNetworkType() : int{
+		return AvailableCommandsPacket::ARG_TYPE_STRING;
+	}
 
-	public function canParse(string $testString, CommandSender $sender): bool
-	{
+	public function getTypeName() : string{
+		return "string";
+	}
+
+	public function canParse(string $testString, CommandSender $sender) : bool{
 		//TODO optimize; TODO check if returning false here triggers argument being passed to another $arg
 		//TODO if regex match '/,(?![^\[]*])/' return true
 		$pattern = '/[\w:]+(?:\[[\w=,]*])?%?\d*/';
